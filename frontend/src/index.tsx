7ee922e756222f6afc2d579f9d330c1e8b829e8d
Goo Zong Lin <zonglingoo@gmail.com>
2025-09-11 16:53:24 +0800

    tailwindcss test #2

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './output.css'; // Import the generated CSS file
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
